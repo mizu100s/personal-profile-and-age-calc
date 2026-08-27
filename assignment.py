4d5cf6369ecdda87b35f92aa0ef271cda0d7ac95
StudentPM <nnewport100s@gmail.com>
2026-08-27 14:14:10 -0700

    First commit

---
+++ assignment.py
@@ -0,0 +1,19 @@
+print("Welcome to the personal profile and age calc")
+print("I'm going to ask you a few questions about you.")
+print("then i'll calculate approximately how long you have been alive")
+
+name= str(input("Enter your name: "))
+favorite_food = str(input("enter your favortie food:"))
+favorite_hobby = str(input("enter your favorite hobby:" ))
+city = str(input("enter the city you live in: "))
+dream_job = str(input("enter your dream job: "))
+favorite_video_game = str(input("enter your favotrite game: "))
+
+Birth_year = int(input("Enter your birth year: "))
+age = 2026 - Birth_year
+
+
+
+
+print("Your age is," + str(age) + "!")
+
