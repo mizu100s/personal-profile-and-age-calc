b854cbf5a8d7188eafcdd1a9a66ad4bc02453822
StudentPM <nnewport100s@gmail.com>
2026-08-28 13:09:32 -0700

    nvm i forgot to put smthn this now the final commit

--- assignment.py
+++ assignment.py
@@ -1,3 +1,8 @@
+#chris
+#PM class
+#personal profile & age calc assignment
+
+
 print("Welcome to the personal profile and age calc")
 print("I'm going to ask you a few questions about you.")
 print("then i'll calculate approximately how long you have been alive")
@@ -27,10 +32,10 @@ print("you enjoy "+ str(favorite_hobby))
 print("your dream job " + str(dream_job))
 print("your favorite video game is " + str(favorite_video_game))
 
-print("Your age is," + str(age) + "!")
-print("you've been alive for" + str(months_lived) + " months!")
-print("you've been alive for" + str(days_lived) + " days!")
-print("you've been alive for" + str(hours_lived) + " hours!")
-print("you've been alive for" + str(seconds) + " seconds!")
+print("Your age is, " + str(age) + "!")
+print("you've been alive for " + str(months_lived) + " months!")
+print("you've been alive for " + str(days_lived) + " days!")
+print("you've been alive for " + str(hours_lived) + " hours!")
+print("you've been alive for " + str(seconds) + " seconds!")
 
 print("thats insane!")
