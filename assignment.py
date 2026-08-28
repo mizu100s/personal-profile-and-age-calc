d50582b59dd818c74867a72a6fc8acd3f6059b3b
StudentPM <nnewport100s@gmail.com>
2026-08-28 13:06:48 -0700

    final changes

--- assignment.py
+++ assignment.py
@@ -11,9 +11,26 @@ favorite_video_game = str(input("enter your favotrite game: "))
 
 Birth_year = int(input("Enter your birth year: "))
 age = 2026 - Birth_year
+months_lived = age * 12
+days_lived = months_lived * 31
+hours_lived = days_lived * 24 
+mins_lived = hours_lived * 60
+seconds = mins_lived * 60 
 
 
 
+print(" -----------" + str(name) + "'s PROFILE ---------")
+print("hello " + str(name)+"!")
+print("your from " + str(city))
+print("your favorite food is " + str(favorite_food))
+print("you enjoy "+ str(favorite_hobby))
+print("your dream job " + str(dream_job))
+print("your favorite video game is " + str(favorite_video_game))
 
 print("Your age is," + str(age) + "!")
+print("you've been alive for" + str(months_lived) + " months!")
+print("you've been alive for" + str(days_lived) + " days!")
+print("you've been alive for" + str(hours_lived) + " hours!")
+print("you've been alive for" + str(seconds) + " seconds!")
 
+print("thats insane!")
